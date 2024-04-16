52d388644bdf39a6741e9d7762ab443209f6e071
Alejo <acastiblanco@hexacta.com>
2024-04-15 21:22:43 -0500

    without build

---
+++ TorcTest.Api/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TorcTest.Application.UseCases;
+
+namespace TorcTest.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly TestIn _testInterface;
+        public HomeController(TestIn testIn)
+        {
+            _testInterface = testIn;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var message = _testInterface.TestString();
+
+            return Ok(message);
+        }
+    }
+}
