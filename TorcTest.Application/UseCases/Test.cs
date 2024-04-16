aae98254c52e52767cf3c264ebbb355031ada621
Alejo <acastiblanco@hexacta.com>
2024-04-15 20:34:35 -0500

    first commit

---
+++ TorcTest.Application/UseCases/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorcTest.Application.UseCases
+{
+    public interface TestIn
+    {
+        string TestString();
+    }
+
+    public class Test: TestIn
+    {
+        public Test()
+        {
+                
+        }
+
+        public string TestString()
+        {
+            return "hello app";
+        }
+    }
+}
